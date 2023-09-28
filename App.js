1b00ccfe5747a7b23d858847d8b42a197fde0f71
williamjparra <w3webservices.net@gmail.com>
2023-09-28 07:22:32 -0400

    new app initalized

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.Text}>starting point</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.Text}>starting point</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 60
-  },
-  Text: {
-    fontSize: 50,
-    textAlign: "center",
-    lineHeight: 65,
-    fontWeight: "bold"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 60,
+	},
+	Text: {
+		fontSize: 50,
+		textAlign: "center",
+		lineHeight: 65,
+		fontWeight: "bold",
+	},
 });
